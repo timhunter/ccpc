8f10fcf70edc949e404e841bc02d4ce8ddaf9810
siw22 <siw22@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-04-17 22:53:42 +0000

    Adding parser.mli

---
+++ parser.mli
@@ -0,0 +1,17 @@
+type item 
+type input= Prefix of (string list) | Sentence of (string list)
+type prim
+type backpointer =  item option * item option
+
+ 
+val deduce: int -> Rule.r list -> input -> item list
+val build_nary: Rule.r list -> item list -> item list
+val rule_arity: Rule.r -> int
+val max_arity: Rule.r list -> int
+val is_goal: input -> item -> bool 
+val get_axioms: Rule.r list -> input -> item list 
+val create_item: string -> (Util.range_item * Util.range_item) list -> backpointer option -> item 
+val get_nonterm: item -> string
+val get_ranges: item -> (Util.range_item * Util.range_item) list 
+val get_backpointer: item -> backpointer option
+val to_string: item -> string
