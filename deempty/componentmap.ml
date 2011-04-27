820c78f0f52e7451b18ebb01159df12e394fb511
jam594 <jam594@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-04-27 04:24:05 +0000

    now remapping component numbers to account for removed components after deemptying

---
+++ deempty/componentmap.ml
@@ -0,0 +1,35 @@
+open Rule
+
+let numcomponentstbl = Hashtbl.create 101
+let mappings = Hashtbl.create 101
+
+let add_original_num (cat,_,sy) =
+  Hashtbl.replace numcomponentstbl cat (List.length sy)
+
+let initialize = List.iter add_original_num
+
+let add_mapping cat nullcomplst =
+  let originalnum = Hashtbl.find numcomponentstbl cat in
+  let rec get_nonnullcomps i ret = 
+    if i<originalnum
+    then 
+      if List.mem i nullcomplst
+      then get_nonnullcomps (i+1) ret
+      else get_nonnullcomps (i+1) (i::ret)
+    else ret in
+  let nonnullcomps = List.rev (get_nonnullcomps 0 []) in
+  let (_,mapping) = List.fold_left (fun (i,acc) x -> ((i+1),((x,i)::acc))) (0,[]) nonnullcomps in
+  Hashtbl.add mappings cat mapping
+
+let compute_mappings nullcomponents_assoclst =
+  List.iter (fun (cat,nullcomps) -> add_mapping cat nullcomps) nullcomponents_assoclst
+
+let lookup cat comp =
+  try
+    List.assoc comp (Hashtbl.find mappings cat)
+  with Not_found -> comp
+
+let rewrite_rule (cat,children,sy) =
+  (cat, children, List.map (fun lst -> (List.map (fun (Component(i,j)) -> Component(i,lookup (List.nth children i) j)) lst)) sy)
+
+let remap_components g = List.map rewrite_rule g
---
+++ deempty/componentmap.mli
@@ -0,0 +1,3 @@
+val initialize : Rule.t list -> unit
+val compute_mappings : (string * int list) list -> unit
+val remap_components : Rule.t list -> Rule.t list
