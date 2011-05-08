20e55e1601cbd42821318b29e89c70cbc13f8f45
siw22 <siw22@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-05-08 19:43:57 +0000

    Refactored some modules

---
+++ chart.mli
@@ -0,0 +1,17 @@
+type backpointer = item option * item option
+and item = ParseItem of string * ((Util.range_item * Util.range_item) list) * backpointer option (*range_item defined in Util*) 
+
+val get_nonterm: item -> string
+val create_item: string -> (Util.range_item * Util.range_item) list -> backpointer option -> item 
+val get_ranges: item -> (Util.range_item * Util.range_item) list 
+val get_backpointer: item -> backpointer option
+val to_string: item -> string 
+type t
+
+val create : int -> t
+val add : t -> item -> unit
+val mem : t -> item -> bool
+val length : t -> int
+val iter : (item -> string -> unit) -> t -> unit
+val find : t -> item -> string
+val fold : (item -> string -> 'a -> 'a) -> t -> 'a -> 'a
