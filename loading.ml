d056b732efddd169c2d745a85083b3fc80826934
del82 <del82@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-05-11 19:34:27 +0000

    added main.mli

---
+++ loading.ml
@@ -0,0 +1,17 @@
+(* #use this file from the interpreter to load the parser, which becomes 
+ * available in module "Main"
+ *)
+
+#load "profiling.cmo";;
+#load "util.cmo";;
+#load "kbest/rational.cmo";;
+#load "nelist.cmo";;
+#load "rule.cmo";;
+#load "mcfgread/read.cmo";;
+#load "mcfgread/lexer.cmo";;
+#load "chart.cmo";;
+#load "tables.cmo";;
+#load "parser.cmo";;
+#load "main.cmo";;
+(* #use "main.ml";;  still doesn't work *)
+   
---
+++ main.mli
@@ -0,0 +1,5 @@
+val main : unit -> unit
+val get_input_grammar : string -> Rule.r list
+val parse : Rule.r list -> string list -> Chart.item list
+val run_parser : string list -> bool -> string option -> string -> unit
+val print_tree : Chart.item -> string list -> out_channel -> unit
