d1db953d9c1b5493559e349aa5520f97e9e0d4f1
del82 <del82@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-05-15 22:38:00 +0000

    updates to the larsonian demo runner.

--- fzqp/larsonian_demo.ml
+++ fzqp/larsonian_demo.ml
@@ -15,8 +15,26 @@ let larsonian_test_sentences = Runner.Util.make_sentence_list
   (Runner.Util.read_file "larsonian.txt")
 
 let n = Runner.Util.range 6
+let m = [7;8;9;10;11;12]
+let o = [13;14;15;16;17;18]
+let p = [19;20;21;22;23]
 
-let test_sentences = List.map (fun x -> List.nth larsonian_test_sentences x) n
+let sents n = List.map (fun x -> List.nth larsonian_test_sentences x) n
 let all_sentences = larsonian_test_sentences  
 
-let results = Runner.runner#parse_all test_sentences
+(* for distributing the job *)
+let first_six = sents n
+let second_six = sents m
+let third_six  = sents o
+let last_five = sents p
+
+let p x = Runner.runner#parse_all x
+(* let results = Runner.runner#parse_all all_sentences *)
+
+let after n list =
+  let rec aux c = function
+      [] -> []
+    | h::t -> if c < n then aux (c+1) t else h::(aux (c+1) t)
+  in
+    aux 0 list
+    
