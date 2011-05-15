92176c5b8490df1a4f2fbd69ff1a2189517c4a2e
siw22 <siw22@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-05-15 14:52:35 +0000

    Fixed timed_parse function which I accidently broke during last commit

--- main.ml
+++ main.ml
@@ -18,7 +18,7 @@ let get_input_grammar grammar_file =
     let channel = open_in grammar_file in 
     let lexbuf = Lexing.from_channel channel in 
     Read.mcfgrule Lexer.token lexbuf  
-  with _ -> print_string "Can't parse input mcfg file\n"; exit 0
+  with _ -> print_string ("Can't parse input mcfg file "^grammar_file^"\n"); []
 
 (******************************************************************************************)
 
@@ -151,6 +151,14 @@ let parse rules symbols =
 (*  MCFG_ParseGen_Deducer.deduce (-1) rules (Parser.Sentence symbols)*)
   Parser.deduce (-1) rules (Parser.Sentence symbols)
 
+let timed_parse rules symbols =
+  let t = Sys.time () in
+  let chart = parse rules symbols in 
+  let duration = (Sys.time ()) -. t in
+  let goal_items = List.filter (Parser.is_goal (Parser.Sentence symbols)) chart in 
+  let d = if (List.length goal_items) > 0 then duration else 0. -. duration in
+    (d, chart)
+
 
 let parse_with_intersection prefix sentence =
   let new_grammar = intersection_grammar (get_input_grammar Sys.argv.(1)) prefix in
@@ -250,6 +258,7 @@ let main =
        | _ -> let sentence = Util.split ' ' Sys.argv.(2) in
               run_parser sentence false Sys.argv.(1);
               ()
-  with _ -> Printf.printf "Usage: mcfg grammar-file (-o output-file) (-d) (-p \"prefix\") \"sentence\"";
-            Printf.printf "\nFlags in parentheses are optional\n"
+  with _ -> Printf.printf "Usage (parse mode): mcfgcky2 grammar-file -o output-file \"sentence\"";
+            Printf.printf "\nUsage (degug mode): mcfgcky2 grammar-file -o output-file -d \"sentence\"";
+            Printf.printf "\nUsage (prefix mode): mcfgcky2 grammar-file -o output-file -p \"prefix\" \"sentence\""
   end
