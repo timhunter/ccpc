3fc90ef7a173bed1d086bba2df4c82be6afc2faf
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-07-12 22:27:57 +0000

    Now using a much smarter system the manage the queue/agenda when generating intersection grammars

--- grammar.ml
+++ grammar.ml
@@ -48,19 +48,36 @@ let new_intersection_grammar_rules prefix chart item =
   let new_agenda_items = concatmap_tr snd results_to_combine in
   (new_rules, new_agenda_items)
 
-let rec build_intersection_grammar prefix chart (agenda,i) grammar_so_far =
-  if (i >= List.length agenda) then
+(********************************************************************)
+(*** A queue with a memory of everything that's ever been in it. ****)
+type 'a myqueue = ('a Queue.t) * (('a,unit) Hashtbl.t)
+let create_myqueue () = (Queue.create (), Hashtbl.create 1000)
+let contains mq x = Hashtbl.mem (snd mq) x
+let pop_myqueue mq = Queue.pop (fst mq)
+let is_empty_myqueue mq = Queue.is_empty (fst mq)
+let add_if_new mq x =
+	if (not (contains mq x)) then (
+		Queue.add x (fst mq) ;
+		Hashtbl.add (snd mq) x ()
+	) else ()
+(********************************************************************)
+
+let rec build_intersection_grammar prefix chart q grammar_so_far =
+  if (is_empty_myqueue q) then
     grammar_so_far
   else
-    let trigger = List.nth agenda i in
+    let trigger = pop_myqueue q in
     let (new_rules, new_agenda_items) = new_intersection_grammar_rules prefix chart trigger in
-    build_intersection_grammar prefix chart (uniques (agenda @ new_agenda_items), i+1) (grammar_so_far @ new_rules)
+    List.iter (fun item -> add_if_new q item) new_agenda_items ;
+    build_intersection_grammar prefix chart q (grammar_so_far @ new_rules)
 
 let intersection_grammar (rules, start_symbol) symbols =
   let chart = Parser.deduce (-1) rules (Parser.Prefix symbols) in
   let goal_items = Chart.goal_items chart start_symbol (List.length symbols) in
+  let q = create_myqueue () in
+  List.iter (fun item -> add_if_new q item) goal_items ;
   let new_start_symbol = Printf.sprintf "%s_0%d" start_symbol (List.length symbols) in
-  let new_rules = build_intersection_grammar symbols chart (goal_items,0) [] in
+  let new_rules = build_intersection_grammar symbols chart q [] in
   (new_rules, new_start_symbol)
 
 
