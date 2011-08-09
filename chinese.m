f2a417abe13f01aa0f4b506364eea61f529e4c0a
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-08-09 21:36:57 +0000

    add chinese stimuli file chinese.m

---
+++ chinese.m
@@ -0,0 +1,4 @@
+chinese = {{"yaoqing","fuhao","de","guanyuan","dale","jizhe"},
+{"fuhao","yaoqing","de","guanyuan","dale","jizhe"},
+{"jizhe","dale","yaoqing","fuhao","de","guanyuan"},
+{"jizhe","dale","fuhao","yaoqing","de","guanyuan"}};
