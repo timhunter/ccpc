43ba4fb9b970bbe7ceb57b1915e9fa9691706907
jth99 <jth99@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-08-10 22:31:27 +0000

    The notebook entropy2-with-oldstyle.nb includes facilities intended to get closer to what the original ocaml stuff did in my dissertation. note that this is not yet working, ie there is still a disparity between these two.

--- grammars/mg/sleep.pl
+++ grammars/mg/sleep.pl
@@ -1,21 +1,25 @@
 [mary]::[n,-f].
-
+[joseph]::[n,-f].
 [john]::[n,-f].
+[matthew]::[n,-f].
+[mark]::[n,-f].
+[luke]::[n,-f].
+
 
 
 [sleeps]::[=n,+f,v].
 
 % recursive postmodification
-[tuesday]::[date].
+% [tuesday]::[date].
 
-[on]::[=date,p_on].
+% [on]::[=date,p_on].
 
 % [from]::[=place,p_from].
 
 % [ohio]::[place].
 
 
-[v]<<[p_on].
+%[v]<<[p_on].
 %[n]<<[p_from].
 
 startCategory(v).
