a656dbce26ac023bcbcc563fc32ba6a7edc5c674
jy249 <jy249@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-08-10 05:12:37 +0000

    added korean.m

---
+++ korean.m
@@ -0,0 +1,8 @@
+korean={{"uywon", "ul", "kongkyekhayssta"},
+{"uywon", "ul", "kongkyekhayse", "kica", "ka", "yumyenghaycyessta"},
+{"uywon", "ul", "kongkyekhan", "sasil", "i", "palkhyecyessta"},
+{"uywon", "ul", "kongkyekhan", "kica", "ka", "yumyenghaycyessta"},
+{"kica", "ka", "kongkyekhayssta"},
+{"kica", "ka", "kongkyekhayse", "uywon", "i", "yumyenghaycyessta"},
+{"kica", "ka", "kongkyekhan", "sasil", "i", "palkhyecyessta"},
+{"kica", "ka", "kongkyekhan", "uywon", "i", "yumyenghaycyessta"}};
