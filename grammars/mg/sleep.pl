fb8f382615a12a4c03bc062ec2151451d2b70744
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-08-12 16:37:39 +0000

    New version of sleep grammar

--- grammars/mg/sleep.pl
+++ grammars/mg/sleep.pl
@@ -1,25 +1,7 @@
 [mary]::[n,-f].
-[joseph]::[n,-f].
 [john]::[n,-f].
-[matthew]::[n,-f].
-[mark]::[n,-f].
-[luke]::[n,-f].
-
-
-
 [sleeps]::[=n,+f,v].
-
-% recursive postmodification
-% [tuesday]::[date].
-
-% [on]::[=date,p_on].
-
-% [from]::[=place,p_from].
-
-% [ohio]::[place].
-
-
-%[v]<<[p_on].
-%[n]<<[p_from].
-
+[tuesday]::[date].
+[on]::[=date,p_on].
+[v]<<[p_on].
 startCategory(v).
