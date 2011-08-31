d01e055d879eb79be7a49804e5c21efe9754d672
jth99 <jth99@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-08-31 15:24:12 +0000

    the function "postprocess" in entropy2-with-oldstyle.nb should be calling oldStyle or newStyle since entOfGrammarFile has gone away. I also put korean.train back to what it was before, the straightforward results of the KTB corpus counts

--- korean.m
+++ korean.m
@@ -6,3 +6,13 @@ korean={{"uywon", "ul", "kongkyekhayssta"},
 {"kica", "ka", "kongkyekhayse", "uywon", "i", "yumyenghaycyessta"},
 {"kica", "ka", "kongkyekhan", "sasil", "i", "palkhyecyessta"},
 {"kica", "ka", "kongkyekhan", "uywon", "i", "yumyenghaycyessta"}};
+
+
+koreanabs={{"Noun", "Acc", "Vdecl"},
+{"Noun", "Acc", "Vadj", "Noun", "Nom", "Vdecl"},
+{"Noun", "Acc", "Vadn", "fact", "Acc", "Vdecl"},
+{"Noun", "Acc", "Vadn", "Noun", "Nom", "Vdecl"},
+{"Noun", "Nom", "Vdecl"},
+{"Noun", "Nom", "Vadj", "Noun", "Acc", "Vdecl"},
+{"Noun", "Nom", "Vadn", "fact", "Acc", "Vdecl"},
+{"Noun", "Nom", "Vadn", "Noun", "Acc", "Vdecl"}};
