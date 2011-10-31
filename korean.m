cbdfe2a08363ae1ea730a6352394552c168e76b8
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-10-31 19:25:44 +0000

    add koreanabs.dict; fixed the koreanabs example in korean.m

--- korean.m
+++ korean.m
@@ -10,9 +10,9 @@ korean={{"uywon", "ul", "kongkyekhayssta"},
 
 koreanabs={{"Noun", "Acc", "Vdecl"},
 {"Noun", "Acc", "Vadj", "Noun", "Nom", "Vdecl"},
-{"Noun", "Acc", "Vadn", "fact", "Acc", "Vdecl"},
+{"Noun", "Acc", "Vadn", "fact", "Nom", "Vdecl"},
 {"Noun", "Acc", "Vadn", "Noun", "Nom", "Vdecl"},
 {"Noun", "Nom", "Vdecl"},
-{"Noun", "Nom", "Vadj", "Noun", "Acc", "Vdecl"},
-{"Noun", "Nom", "Vadn", "fact", "Acc", "Vdecl"},
-{"Noun", "Nom", "Vadn", "Noun", "Acc", "Vdecl"}};
+{"Noun", "Nom", "Vadj", "Noun", "Nom", "Vdecl"},
+{"Noun", "Nom", "Vadn", "fact", "Nom", "Vdecl"},
+{"Noun", "Nom", "Vadn", "Noun", "Nom", "Vdecl"}};
