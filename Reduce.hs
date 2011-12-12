5d5f3028cdb600a5aec193935ecd18add2d1a9c3
cs872 <cs872@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2011-12-12 04:04:45 +0000

    Try to parallelize mapReduce a bit

--- Reduce.hs
+++ Reduce.hs
@@ -3,19 +3,23 @@
 module Reduce (reduce, mapReduce) where
 
 import Data.Monoid
-import Data.Maybe (catMaybes)
 import Data.List (foldl')
+import Control.Parallel (par)
 
 -- MapReduce over a long list
 
 reduce :: Monoid a => [a] -> a
-reduce []     = mempty
-reduce [x]    = x
-reduce (x:xs) = reduce (reverse (catMaybes (foldl' (shift 0) [Just x] xs)))
-  where shift n _             x | n `seq` x `seq` False = undefined
-        shift n []            x = replicate n Nothing ++ Just x : []
-        shift n (Nothing: ys) x = replicate n Nothing ++ Just x : ys
-        shift n (Just y : ys) x = shift (succ n) ys (mappend y x)
+reduce []       = mempty
+reduce [x]      = x
+reduce (x:y:xs) = reduce (reverse (map snd
+                    (foldl' (shift 0) [(1, mappend x y)] xs)))
+
+shift :: Monoid a => Int -> [(Int,a)] -> a -> [(Int,a)]
+shift n _          x | (case compare n 9 of { LT -> id; EQ -> par x; GT ->
+                        if n >= 12 then seq x else id }) False = undefined
+shift n []         x = [(n,x)]
+shift n ((0,y):ys) x = shift (succ n) ys (mappend y x)
+shift n ((m,y):ys) x = (n,x):(pred m,y):ys
 
 mapReduce :: Monoid a => (b -> a) -> [b] -> a
 mapReduce f = reduce . map f
