c42478caaca3db3413f94a473567d087da1f5eb7
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-03-28 22:45:31 +0000

    add ChinesepromtionalNnulldetcl grammar

--- chinese.m
+++ chinese.m
@@ -6,4 +6,9 @@ chinese = {{"yaoqing","fuhao","de","guanyuan","dale","jizhe"},
 chineseabs = {{"Vt","Noun","de","Noun","Vt","Noun"},
 {"Noun","Vt","de","Noun","Vt","Noun"},
 {"Noun","Vt","Vt","Noun","de","Noun"},
-{"Noun","Vt","Noun","Vt","de","Noun"}};
+{"Noun","Vt","Noun","Vt","de","Noun"},
+{"Det","Cl","Time","Vt","Noun","Freq","de","Noun","Vt","Noun"},
+{"Det","Cl","Time","Noun","Vt","Freq","de","Noun","Vt","Noun"},
+{"Noun","Vt","Det","Cl","Time","Vt","Noun","Freq","de","Noun"},
+{"Noun","Vt","Det","Cl","Time","Noun","Vt","Freq","de","Noun"}
+};
