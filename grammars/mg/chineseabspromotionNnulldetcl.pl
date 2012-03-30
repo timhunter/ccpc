912e3fce96f3cd0a9ddcb9e0781b8bc5e0ce53e7
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-03-30 11:55:21 +0000

    updated the chineseabspromotionNnulldetcl.pl with specified SR and OR

--- grammars/mg/chineseabspromotionNnulldetcl.pl
+++ grammars/mg/chineseabspromotionNnulldetcl.pl
@@ -10,21 +10,25 @@
 % Null Determiners
  []::[='N','D',-case].
  []::[='N-null','D-null',-case].   % pro also has a null D
- []::[='N-Rel','D',-case]. % NP-Rel could have a null D
+ []::[='N-SR','D-SR',-case]. % NP-SR could have a null D
+ []::[='N-OR','D-OR',-case]. % NP-OR could have a null D
 
 % Overt Determiners, such as "zhe", "na".
 % It needs to work with a classifer in Chinese.
  ['Det']::[='CL','D',-case].
- ['Det']::[='CL-Rel','D-Rel',-case].
+ ['Det']::[='CL-SR','D-SR',-case].
+ ['Det']::[='CL-OR','D-OR',-case].
+% ['Det']::[='CL-Rel','D-Rel'].  % D-Rel as possessor
 
 % Classifiers
  ['Cl']::[='N','CL'].
- ['Cl']::[='N-Rel','CL-Rel'].
+ ['Cl']::[='N-SR','CL-SR'].
+ ['Cl']::[='N-OR','CL-OR'].
 
 % Nouns
  ['Noun']::['N'].	     % in possessives
  ['Noun']::['N',-case].
- ['Noun']::['N',-case,-wh].  % nouns that can be relativized
+ ['Noun']::['N-Rel',-case,-wh].  % nouns that can be relativized
 
 % Null argument (pro)
  []::['N-null'].            %JTH
@@ -38,14 +42,18 @@
  ['Vt']::[='Poss',+case,'V'].	% take possessive as arguments
  ['Vt']::[='D',+case,'V']. % for Det
  ['Vt']::[='D-null',+case,'V'].
- ['Vt']::[='D-Rel',+case,'V']. % RC as matrix object
+ ['Vt']::[='D-SR',+case,'V']. % SR as matrix object
+ ['Vt']::[='D-OR',+case,'V']. % OR as matrix object
+
+
 
 % Transitive verbs allow duration/freq phrases.
 % Huang et al (2009) treats FP as the adjunct of V'.
 % We implement it as complement of Vt and raise it (+/-m) for word order purposes.
  ['Vt']::[='F',+m,='Poss',+case,'V'].	% take possessive as arguments
  ['Vt']::[='F',+m,='D',+case,'V']. % for Det
- ['Vt']::[='F',+m,='D-Rel',+case,'V']. % for Det
+ ['Vt']::[='F',+m,='D-SR',+case,'V']. % for Det
+ ['Vt']::[='F',+m,='D-OR',+case,'V']. % for Det
 
  ['Freq']::['F',-m].
 
@@ -53,7 +61,8 @@
  []::[=>'V',='Poss','v'].
  []::[=>'V',='D','v']. 
  []::[=>'V',='D-null','v'].
- []::[=>'V',='D-Rel','v']. % RC as matrix subject
+ []::[=>'V',='D-SR','v']. % SR as matrix subject
+ []::[=>'V',='D-OR','v']. % OR as matrix subject
 
 % tense
  []::[='v',+case,'T']. %tense
@@ -70,37 +79,51 @@
 % OR:   showParse([Det,Cl,Time,Noun,Vt,Freq,de,Noun,Vt,Noun]).
 
 
- ['Vt']::[='N',+case,'V-Rel'].
 %['Vt']::[='N-null',+case,'V-Rel'].  % obj-pro-drop is not allowed in RCs
- ['Vt']::[='N-null-Rel',+case,'V-Rel'].
- ['Vt']::[='Poss',+case,'V-Rel'].
-
- ['Vt']::[='F',+m,='N',+case,'V-Rel'].
- ['Vt']::[='F',+m,='N-null-Rel',+case,'V-Rel'].
-
- ['Vi']::['V-Rel'].
+ ['Vt']::[='N-null-Rel',+case,'V-OR'].
+ ['Vt']::[='Poss',+case,'V-SR'].
+ ['Vt']::[='N-Rel',+case,'V-OR'].
+ ['Vt']::[='N',+case,'V-SR'].
 
 
- []::[=>'V-Rel',='N','vRel'].         %v
- []::[=>'V-Rel',='N-null','vRel'].    % subj-pro-drop is permitted in RCs
- []::[=>'V-Rel',='N-null-Rel','vRel'].  
- []::[=>'V-Rel',='Poss','vRel'].
+ ['Vt']::[='F',+m,='N',+case,'V-SR'].
+ ['Vt']::[='F',+m,='N-null-Rel',+case,'V-OR'].
+ ['Vt']::[='F',+m,='Poss',+case,'V-SR'].
+ ['Vt']::[='F',+m,='N-Rel',+case,'V-OR'].
 
- []::[='vRel',+case,'TRel',-f].	%tense
+ ['Vi']::['V-SR'].
+
+
+ []::[=>'V-OR',='N','v-OR'].         %v
+ []::[=>'V-OR',='N-null','v-OR'].    % subj-pro-drop is permitted in RCs
+ []::[=>'V-OR',='Poss','v-OR'].
+ []::[=>'V-SR',='N-null-Rel','v-SR'].  
+ []::[=>'V-SR',='N-Rel','v-SR'].
+
+ []::[='v-SR',+case,'T-SR',-f].	%tense
+ []::[='v-OR',+case,'T-OR',-f].	%tense
+
 
 % relativizer de hoist TRel to Spec CP
- [de]::[='TRel',+f,'CRel',-k].
+ [de]::[='T-SR',+f,'C-SR',-k].
+ [de]::[='T-OR',+f,'C-OR',-k].
 
- ['Temp']>>['TRel'].
+
+ ['Temp']>>['T-SR'].
+ ['Temp']>>['T-OR'].
 
 % raise argument NP to head position
- []::[='CRel',+wh,'N'].
+ []::[='C-SR',+wh,'N-SR'].
+ []::[='C-OR',+wh,'N-OR'].
 
 % need to move CP to Spec NP
- []::[='N',+k,'N-Rel'].
+ []::[='N-SR',+k,'N-SR'].
+ []::[='N-OR',+k,'N-OR'].
 
 % possessive de
  [de]::[='N',='N','Poss',-case].
+ [de]::[='N',='D-SR','Poss',-case].
+ [de]::[='N',='D-OR','Poss',-case].
 
 % The root.
 startCategory('C').
