6dbbcc889fcf7dbc8fed02aa0260b5a6c1b009af
jy249 <jy249@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-07-17 20:36:38 +0000

    japanese without clefts

--- japanese.m
+++ japanese.m
@@ -1,8 +1,6 @@
 japanese = {
-{"N","o","Vt","N","ga","N","o","Vt"},
-{"N","ga","Vt","N","ga","N","o","Vt"},
-{"N","o","Vt","no","wa","N","da"},
-{"N","ga","Vt","no","wa","N","da"},
-{"N","o","Vt","no","ga","N","o","Vt"},
-{"N","ga","Vt","no","ga","N","o","Vt"}
+{"N","o","Vt","N","ga","Vi"},
+{"N","ga","Vt","N","ga","Vi"},
+{"N","o","Vt","no","ga","Vi"},
+{"N","ga","Vt","no","ga","Vi"}
 }
