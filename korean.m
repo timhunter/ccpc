9a3810a42dc16842dcf779c0d9cf55c49ffd6972
jy249 <jy249@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-07-17 17:56:55 +0000

    updated korean.m according to the grammar

--- korean.m
+++ korean.m
@@ -1,14 +1,4 @@
-korean={{"uywon", "ul", "kongkyekhayssta"},
-{"uywon", "ul", "kongkyekhayse", "kica", "ka", "yumyenghaycyessta"},
-{"uywon", "ul", "kongkyekhan", "sasil", "i", "palkhyecyessta"},
-{"uywon", "ul", "kongkyekhan", "kica", "ka", "yumyenghaycyessta"},
-{"kica", "ka", "kongkyekhayssta"},
-{"kica", "ka", "kongkyekhayse", "uywon", "i", "yumyenghaycyessta"},
-{"kica", "ka", "kongkyekhan", "sasil", "i", "palkhyecyessta"},
-{"kica", "ka", "kongkyekhan", "uywon", "i", "yumyenghaycyessta"}};
-
-
-koreanabs={{"Noun", "Acc", "Vdecl"},
+korean={{"Noun", "Acc", "Vdecl"},
 {"Noun", "Acc", "Vadj", "Noun", "Nom", "Vdecl"},
 {"Noun", "Acc", "Vadn", "fact", "Nom", "Vdecl"},
 {"Noun", "Acc", "Vadn", "Noun", "Nom", "Vdecl"},
@@ -16,9 +6,3 @@ koreanabs={{"Noun", "Acc", "Vdecl"},
 {"Noun", "Nom", "Vadj", "Noun", "Nom", "Vdecl"},
 {"Noun", "Nom", "Vadn", "fact", "Nom", "Vdecl"},
 {"Noun", "Nom", "Vadn", "Noun", "Nom", "Vdecl"}};
-
-koreanabsrc={
-{"Noun", "Acc", "Vadn", "Noun", "Nom", "Noun", "Acc", "VTDecl"},
-{"Noun", "Nom", "Vadn", "Noun", "Nom", "Noun", "Acc", "VTDecl"},
-{"Noun", "Nom", "Noun", "Acc", "Vadn", "Noun", "Acc", "VTDecl"},
-{"Noun", "Nom", "Noun", "Nom", "Vadn", "Noun", "Acc", "VTDecl"}};
