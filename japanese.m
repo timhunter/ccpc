ba56add5c9dc5f03fa66de89120f6d1536bff1cd
jy249 <jy249@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-07-17 20:45:19 +0000

    japanese.m with vt only

--- japanese.m
+++ japanese.m
@@ -1,6 +1,6 @@
 japanese = {
-{"N","o","Vt","N","ga","Vi"},
-{"N","ga","Vt","N","ga","Vi"},
-{"N","o","Vt","no","ga","Vi"},
-{"N","ga","Vt","no","ga","Vi"}
+{"N","o","Vt","N","ga","N","o","Vt"},
+{"N","ga","Vt","N","ga","N","o","Vt"},
+{"N","o","Vt","no","ga","N","o","Vt"},
+{"N","ga","Vt","no","ga","N","o","Vt"}
 }
