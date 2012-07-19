3067f1735bc19b956183327982e163078f5ea85e
jy249 <jy249@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-07-19 20:06:35 +0000

    test file with transitive verbs

--- korean.m
+++ korean.m
@@ -1,8 +1,8 @@
 korean={
-{"N", "acc", "Vtn", "N", "nom", "Vid"},
-{"N", "nom", "Vtn", "N", "nom", "Vid"},
-{"N", "acc", "Vtn", "fact", "nom", "Vid"},
-{"N", "nom", "Vtn", "fact", "nom", "Vid"},
-{"N", "acc", "Vta", "N", "nom", "Vid"},
-{"N", "nom", "Vta", "N", "nom", "Vid"},
+{"N", "acc", "Vtn", "N", "nom", "N", "acc", "Vtd"},
+{"N", "nom", "Vtn", "N", "nom", "N", "acc", "Vtd"},
+{"N", "acc", "Vtn", "fact", "nom", "N", "acc", "Vtd"},
+{"N", "nom", "Vtn", "fact", "nom", "N", "acc", "Vtd"},
+{"N", "acc", "Vta", "N", "nom", "N", "acc", "Vtd"},
+{"N", "nom", "Vta", "N", "nom", "N", "acc", "Vtd"}
 };
