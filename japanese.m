fbadd47fe1db97b018858773653a31e9ec27023b
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-07-20 18:22:47 +0000

    unnecessary chinese grammars removed

--- japanese.m
+++ japanese.m
@@ -2,5 +2,7 @@ japanese = {
 {"N","o","Vt","N","ga","N","o","Vt"},
 {"N","ga","Vt","N","ga","N","o","Vt"},
 {"N","o","Vt","no","ga","N","o","Vt"},
-{"N","ga","Vt","no","ga","N","o","Vt"}
+{"N","ga","Vt","no","ga","N","o","Vt"},
+{"N","o","Vt","N","ga","Vi"},
+{"N","ga","Vt","N","ga","Vi"}
 }
