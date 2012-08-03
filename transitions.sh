8507bb1bc480338b359995ac43fbe19c53da50c7
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-08-03 06:07:53 +0000

    Added transitions.sh script, which runs visualisation for ALL prefixes of a given string; it produces a pdf file for each prefix, and gathers together the listings of surface strings into an additional tex file

---
+++ transitions.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Given a grammar and a full sentence, this script runs visualization of 
+# the intersection of the grammar with each prefix of the sentence. 
+# It produces (a) a pdf file for each prefix, and
+#             (b) a tex file bringing together the tables of surfaces strings 
+#                 at each prefix.
+
+if [ $# -ne 2 ] ; then
+        echo "Usage: $0 <grammar-file> <sentence>" ;
+        exit 1
+fi
+
+grammar=$1
+sentence=$2
+
+if [ ! -f grammars/wmcfg/$grammar.wmcfg ] ; then
+        echo "File grammars/wmcfg/$grammar.wmcfg does not exist"
+        exit 1
+fi
+
+echo "============================"
+echo "Starting `basename $0`"
+echo "Grammar is: $grammar"
+echo "Sentence is: $sentence"
+echo "============================"
+
+function get_prefixes () {
+        echo $1 | awk '
+                {
+                        for (i=0; i<=NF; i++) {
+                                prefix="";
+                                for (j=1; j<=i; j++) {
+                                        prefix = prefix " " $j;
+                                }
+                                sub(/^ /, "", prefix);
+                                print prefix;
+                        }
+                }
+        '
+}
+
+function get_tables () {
+        cat $1 | awk '
+                BEGIN { x=0 }
+                {
+                        if ($0 ~ /\\begin{table}/) {x=1;}
+                        if (x==1) {print $0;}
+                        if ($0 ~ /\\end{table}/) {x=0; print "";}
+                }
+        '
+}
+
+function no_spaces () {
+        echo $1 | sed 's/ /-/g'
+}
+
+tables_file=$grammar.`no_spaces "$sentence"`.combined.$$.tex
+
+get_prefixes "$sentence" |\
+while read prefix ; do
+        prefix_no_spaces=`no_spaces "$prefix"`
+        id=/tmp/$grammar.$prefix_no_spaces.$$
+        ./mcfg_nt grammars/wmcfg/$grammar.wmcfg -intersect -p "$prefix" > $id.chart
+        cp $id.chart $id.global.chart    # TODO: Actually renormalize here!
+        ./visualize $id.chart 100 $id.tex >/dev/null
+        pdflatex $id.tex >/dev/null
+        echo "*** Created pdf file: `basename $id`.pdf"
+        get_tables $id.tex >> $tables_file
+        rm -f $id.chart $id.global.chart `basename $id`.aux `basename $id`.log
+done
+
+echo "============================"
+echo "Tables for all prefixes are collected in $tables_file"
+echo "Exiting `basename $0`"
+echo "============================"
