6b2fa1bb048d293ab6f990bc5f8fde9f3c7a369b
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-10-08 17:55:51 +0000

    Testing script can now accept command-line args to specify which tests to run

--- testing/run.sh
+++ testing/run.sh
@@ -3,17 +3,30 @@
 OUT=`mktemp /tmp/out.XXXX`
 ERR=`mktemp /tmp/err.XXXX`
 
-for f in test*.sh ; do
+cd `dirname $0`
+
+function run_test () {
+        f=$1
+        echo -en "$f : \t"
         pushd .. >/dev/null
         source testing/$f 1>$OUT 2>$ERR
         popd >/dev/null
-        echo -en "$f : \t"
         if (diff `basename $f .sh`.out $OUT >/dev/null) && (diff `basename $f .sh`.err $ERR >/dev/null) ; then
                 echo "PASS!"
         else
                 echo "FAIL!"
         fi
-done
+}
+
+if [[ $# -eq 0 ]] ; then
+        for f in test*.sh ; do
+                run_test $f
+        done
+else
+        for x in $@ ; do
+                run_test $x
+        done
+fi
 
 rm $OUT $ERR
 
