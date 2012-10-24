d76bce1f93d95304be162948ab71c9eadb332563
zc77 <zc77@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-10-24 02:43:58 +0000

    deleting some redundant nb and m files in root; they were moved to grammars/test

--- ah-singular-abs1.m
+++
@@ -1,6 +0,0 @@
-keenanhawkinsabs2 = {"the","story","which","tell","-ed","the","story","be","-s","so","young"},
-{"the","story","which","John","tell","-ed","yesterday","be","-s","so","young"},
-{"the", "story", "which", "John", "tell", "-ed", "the", "story", "to", "tell", "-ed", "John"},
-{"the", "story", "which", "John", "tell", "-ed", "the", "story", "with", "be", "-s", "young"},
-{"the", "story", "which", "s", "story", "tell", "-ed", "the", "story", "be", "-s", "matter", "-ing"},
-{"the", "story", "which", "s", "story", "John", "tell", "-ed", "be", "-s", "so", "young"}}; 
--- subjobj.m
+++
@@ -1,4 +0,0 @@
-subjobj = {{"the","reporter","who","sent","the","photographer","to","the","editor","hoped","for","a","good","story"},
-	   {"the","reporter","who","the","photographer","sent","to","the","editor","hoped","for","a","good","story"}
-	  }
-		  
