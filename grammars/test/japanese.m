4a39ddf3e68937b3a7b4181f7ea9d9dcefa36e66
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-10-25 18:03:46 +0000

    Removing grammars and results related to CJK paper. These have just been added to the cjk repository on yngve (r8).

--- grammars/test/japanese.m
+++
@@ -1,6 +0,0 @@
-japanese = {
-{"N","Acc","Vt","N","Nom","N","Acc","Vt"},
-{"N","Nom","Vt","N","Nom","N","Acc","Vt"},
-{"N","Acc","Vt","fact","Nom","N","Acc","Vt"},
-{"N","Nom","Vt","fact","Nom","N","Acc","Vt"}
-};
--- grammars/test/korean.m
+++
@@ -1,8 +0,0 @@
-korean={
-{"N", "Acc", "Vtn", "N", "Nom", "N", "Acc", "Vtd"},
-{"N", "Nom", "Vtn", "N", "Nom", "N", "Acc", "Vtd"},
-{"N", "Acc", "Vtn", "fact", "Nom", "N", "Acc", "Vtd"},
-{"N", "Nom", "Vtn", "fact", "Nom", "N", "Acc", "Vtd"},
-{"N", "Acc", "Vta", "N", "Nom", "N", "Acc", "Vtd"},
-{"N", "Nom", "Vta", "N", "Nom", "N", "Acc", "Vtd"}
-};
