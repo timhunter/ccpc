61dfa455f73cb67697da425146764efe1a2bd797
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2012-10-31 17:20:00 +0000

    Removed fzqp directory (something David Lutz was doing at some point with the Stanford parser)

--- fzqp/del82-lexparser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Runs the English PCFG parser on one or more files, printing trees only
-#
-scriptdir="/home/del82/projects/mcfgcky/stanford-parser-2010-11-30"
-java -mx150m -cp "$scriptdir/stanford-parser.jar:$scriptdir/*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -outputFormat "penn" $scriptdir/englishPCFG.ser.gz $*
