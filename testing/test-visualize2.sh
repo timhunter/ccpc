42209cf0f906cc469f2e49bbd3dd543bf0fbb9ac
thunter <timhunter@users.noreply.github.com>
2013-07-07 03:50:31 +0000

    Modified a random-sampling test-case to use a sample size that places the cut-off between two unequally-weighted derivations (so that changes in ordering among equally-weighted derivations won't change the set of derivations that are output)

--- testing/test-visualize2.sh
+++ testing/test-visualize2.sh
@@ -1,6 +1,6 @@
 
 TMP=`mktemp /tmp/foo.XXXX`
-./visualize -sample grammars/wmcfg/strauss.wmcfg 33 $TMP 0
+./visualize -sample grammars/wmcfg/strauss.wmcfg 25 $TMP 0
 echo "======================================"
 cat $TMP | egrep -v '\\item timestamp: '
 rm $TMP
