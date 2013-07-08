6f580a215c777fc49a3914c4a9d3be93efdef805
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2013-07-08 22:27:29 +0000

    Factored canonicalize functionality out of individual testing scripts

---
+++ trunk/testing/canonicalize.sh
@@ -0,0 +1,7 @@
+
+# Canonicalizes a kbest list by prepending a rank to each line (where lines 
+# with equal weights have equal ranks) and then sorting numerically. This has 
+# the effect of sorting each set of equally-weighted derivations, but leaving 
+# relative ordering among non-equally-weighted derivations intact.
+awk '{if ($1 != w) {w = $1; rank=NR} ; print rank, $0;}' | LC_COLLATE=C sort -n
+
--- trunk/testing/test7.sh
+++ trunk/testing/test7.sh
@@ -1,10 +1,2 @@
 
-# Canonicalizes a kbest list by prepending a rank to each line (where lines 
-# with equal weights have equal ranks) and then sorting numerically. This has 
-# the effect of sorting each set of equally-weighted derivations, but leaving 
-# relative ordering among non-equally-weighted derivations intact.
-function canonicalize () {
-    awk '{if ($1 != w) {w = $1; rank=NR} ; print rank, $0;}' | LC_COLLATE=C sort -n
-}
-
-./mcfg_nt grammars/wmcfg/strauss.wmcfg -kbest 58 -p "Jon" | canonicalize
+./mcfg_nt grammars/wmcfg/strauss.wmcfg -kbest 58 -p "Jon" | `dirname $0`/canonicalize.sh
