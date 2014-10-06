97d7592d79e93eb7279fa0bb322cf384f33190c9
jth99 <11217592+jthale76@users.noreply.github.com>
2014-10-06 17:26:19 +0000

    removed the wrapper script around call to renormalize.m transitions.sh handles the only postprocessing needed, changing EMPTY into a quoted pair of spaces I think this is due to Wolfram's having fixed bugs in MathematicaScript's i/o

--- transitions.sh
+++ transitions.sh
@@ -90,13 +90,13 @@ tables_file=/tmp/`basename $grammar .wmcfg`.$tag.`no_spaces "$sentence"`.tables.
 entropies_file=/tmp/`basename $grammar .wmcfg`.$tag.`no_spaces "$sentence"`.entropies.$$.tex
 combined_file=`basename $grammar .wmcfg`.$tag.`no_spaces "$sentence"`.combined.$$.tex
 
-renormalizer=./renormalize.csh
+renormalizer=./renormalize.m
 
 echo "$sentence" | get_prefixes |\
 while read prefix ; do
         id=/tmp/`basename $grammar .wmcfg`.$tag.`no_spaces "$prefix"`.$$
         ./intersect -g $grammar -prefix "$prefix" > $id.chart
-        $renormalizer $id.chart > $id.global.chart
+        $renormalizer $id.chart | sed -f twospaces.sed > $id.global.chart # sed call used to be in wrapper, which we no longer use
         echo -e "`egrep -o "entropy = [-+]?[0-9]*\.?[0-9]*([eE][-+]?[0-9]+)?" $id.global.chart | cut -d ' ' -f 3` \t $prefix" >> $entropies_file
         if [ "$mode" == "-sample" ] ; then
             seed_arg="-seed $$"  # use $$, which also appears in output filenames, as random seed
