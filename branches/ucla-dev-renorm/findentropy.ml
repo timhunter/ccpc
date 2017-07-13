6de0035e8ecda31ad941235f35e0f961d2309cab
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2017-07-13 17:32:30 +0000

    Added dummy code for manipulating rules to renormalize.ml and findentropy.ml

--- branches/ucla-dev-renorm/findentropy.ml
+++ branches/ucla-dev-renorm/findentropy.ml
@@ -1,11 +1,22 @@
 
 open Util
+open Rule
 
 (* All the work will go here. *)
 (* The arguments are an existing grammar (list of rules) to be normalized and that grammar's start symbol. 
  * The return value is the entropy of the start symbol in that grammar.
  *)
 let find_entropy rules start_symbol =
+    (*** dummy code showing manipulation of rules ***)
+    List.iter (fun r ->
+        (* get_nonterm and get_weight are from rule.ml; float_of_weight is from util.ml, see also other weight functions there *)
+        Printf.printf "This rule expands %s with weight %f\n" (get_nonterm r) (float_of_weight (get_weight r)) ;
+        match (get_expansion r) with
+        | PublicTerminating str -> Printf.printf "   and the right-hand side is the terminal '%s'\n" str
+        | PublicNonTerminating (nts,_) -> Printf.printf "   and the right-hand side has nonterminals %s\n" (show_list (fun x -> x) (Nelist.to_list nts))
+                                                                                                            (* show_list is another util.ml function *)
+    ) rules ;
+    (*** end dummy code ***)
     let m = Matrix.create_square_matrix 3 (fun r -> fun c -> 0.0) in
     let m' = Matrix.invert m in
     0.0
--- branches/ucla-dev-renorm/renormalize.ml
+++ branches/ucla-dev-renorm/renormalize.ml
@@ -1,5 +1,6 @@
 
 open Util
+open Rule
 
 (* All the work will go here. *)
 (* The arguments are an existing grammar (list of rules) to be normalized and that grammar's start symbol. 
@@ -7,6 +8,16 @@ open Util
  * symbol in the argument grammar, and the second member is the list of rules with reweighted probabilities. 
  *)
 let renormalize_grammar rules start_symbol =
+    (*** dummy code showing manipulation of rules ***)
+    List.iter (fun r ->
+        (* get_nonterm and get_weight are from rule.ml; float_of_weight is from util.ml, see also other weight functions there *)
+        Printf.printf "This rule expands %s with weight %f\n" (get_nonterm r) (float_of_weight (get_weight r)) ;
+        match (get_expansion r) with
+        | PublicTerminating str -> Printf.printf "   and the right-hand side is the terminal '%s'\n" str
+        | PublicNonTerminating (nts,_) -> Printf.printf "   and the right-hand side has nonterminals %s\n" (show_list (fun x -> x) (Nelist.to_list nts))
+                                                                                                            (* show_list is another util.ml function *)
+    ) rules ;
+    (*** end dummy code ***)
     (0.0, rules)
 
 let main () =
