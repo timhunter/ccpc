863c4873ea84b61e8121b295ea367725c1fd7d02
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2017-08-02 16:34:44 +0000

    Small changes and additions to matrix interface

--- branches/ucla-dev-renorm/matrix.ml
+++ branches/ucla-dev-renorm/matrix.ml
@@ -17,17 +17,24 @@ let create_square_matrix n f =
     let list_of_columns = map_tr create_row indices in
     OCamlMatrix.Matrix.EltMatrix.from_list list_of_columns
 
+let get_element m r c =
+    float_of_elt (OCamlMatrix.Matrix.EltMatrix.get_elt m (r,c))
+
 let invert = OCamlMatrix.Matrix.EltMatrix.inverse
 
 let multiply = OCamlMatrix.Matrix.EltMatrix.mult
 
 let mult_vec_by xs m =
+    let indices = map_tr (fun x -> x + 1) (range 0 (List.length xs)) in
     let row_vector = OCamlMatrix.Matrix.EltMatrix.from_list [map_tr elt_of_float xs] in
-    multiply row_vector m
+    let m_result = multiply row_vector m in
+    map_tr (fun c -> get_element m_result 1 c) indices
 
 let mult_by_vec m xs =
+    let indices = map_tr (fun x -> x + 1) (range 0 (List.length xs)) in
     let col_vector = OCamlMatrix.Matrix.EltMatrix.from_list (map_tr (fun x -> [elt_of_float x]) xs) in
-    multiply m col_vector
+    let m_result = multiply m col_vector in
+    map_tr (fun r -> get_element m_result r 1) indices
 
 let print m =
     let (num_rows, num_cols) = OCamlMatrix.Matrix.EltMatrix.get_dimensions m in
--- branches/ucla-dev-renorm/matrix.mli
+++ branches/ucla-dev-renorm/matrix.mli
@@ -7,6 +7,10 @@ type matrix
     which has the value [f i j] at position [i,j]. *)
 val create_square_matrix : int -> (int -> int -> float) -> matrix
 
+(** Returns the element at the specified position. The first argument is the zero-indexed row number, 
+    the second argument is the zero-indexed column number. *)
+val get_element : matrix -> int -> int -> float
+
 (** Inverts a matrix. *)
 val invert : matrix -> matrix
 
@@ -15,11 +19,11 @@ val multiply : matrix -> matrix -> matrix
 
 (** Multiplies a column vector, given in the form of a list, by a matrix. 
     The result is another column vector. *)
-val mult_vec_by : float list -> matrix -> matrix
+val mult_vec_by : float list -> matrix -> float list
 
 (** Multiplies a matrix by a row vector, given in the form of a list.
     The result is another row vector. *)
-val mult_by_vec : matrix -> float list -> matrix
+val mult_by_vec : matrix -> float list -> float list
 
 (** Prints out a matrix. Used for debugging. *)
 val print : matrix -> unit
