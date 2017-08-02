c212b256d8d3d81949e5c80453610b695854abfe
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2017-08-02 00:16:34 +0000

    Added new functions (print and multiply) to matrix interface, and dummy code using them in findentropy.ml

--- branches/ucla-dev-renorm/matrix.mli
+++ branches/ucla-dev-renorm/matrix.mli
@@ -3,3 +3,8 @@ type matrix
 val create_square_matrix : int -> (int -> int -> float) -> matrix
 
 val invert : matrix -> matrix
+
+val multiply : matrix -> matrix -> matrix
+
+val print : matrix -> unit
+
