77e6aa32e99a8393d0cca7f946d5ddad804cb35b
thunter <timhunter@users.noreply.github.com>
2017-08-02 00:52:21 +0000

    Better matrix print function. (The one implemented in ocaml-matrix put funny newlines everywhere.)

--- matrix.ml
+++ matrix.ml
@@ -2,9 +2,17 @@ open Util
 
 type matrix = OCamlMatrix.Matrix.EltMatrix.matrix
 
+(************************************************************)
+(* Private functions for use inside this file ***************)
+
+let elt_of_float x = OCamlMatrix.Elts.Elts.from_string (string_of_float x)
+
+let float_of_elt x = float_of_string (OCamlMatrix.Elts.Elts.to_string x)
+
+(************************************************************)
+
 let create_square_matrix n f =
     let indices = range 0 n in
-    let elt_of_float x = OCamlMatrix.Elts.Elts.from_string (string_of_float x) in   (* what a mess *)
     let create_row r = map_tr (fun c -> elt_of_float (f r c)) indices in
     let list_of_columns = map_tr create_row indices in
     OCamlMatrix.Matrix.EltMatrix.from_list list_of_columns
@@ -13,4 +21,12 @@ let invert = OCamlMatrix.Matrix.EltMatrix.inverse
 
 let multiply = OCamlMatrix.Matrix.EltMatrix.mult
 
-let print = OCamlMatrix.Matrix.EltMatrix.print
+let print m =
+    let (num_rows, num_cols) = OCamlMatrix.Matrix.EltMatrix.get_dimensions m in
+    let go r c elt =
+        if c = 1 then Printf.printf "| " else () ;
+        Printf.printf "% f " (float_of_elt elt) ;
+        if c = num_cols then Printf.printf "|\n" else () ;
+    in
+    OCamlMatrix.Matrix.EltMatrix.iteri go m
+
