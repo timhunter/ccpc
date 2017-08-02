20fabde442dd6e028f4ff47f8ed7de4711ca9136
thunter <timhunter@users.noreply.github.com>
2017-08-02 01:03:21 +0000

    Added functions for multiplying matrices by vectors

--- matrix.mli
+++ matrix.mli
@@ -6,5 +6,9 @@ val invert : matrix -> matrix
 
 val multiply : matrix -> matrix -> matrix
 
+val mult_vec_by : float list -> matrix -> matrix
+
+val mult_by_vec : matrix -> float list -> matrix
+
 val print : matrix -> unit
 
