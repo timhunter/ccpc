fb247cbef1047cc2f591e8e03bad442d56406a7f
thunter <thunter@e18ef4d8-a99d-0410-a4b2-e56463162ca9>
2017-09-08 19:35:03 +0000

    OOOPS! Undoing previous mistaken commit. This is equivalent to r816.

--- branches/ucla-dev-renorm/mgcky-swi/tree_display/latex_treeSWI.pl
+++ branches/ucla-dev-renorm/mgcky-swi/tree_display/latex_treeSWI.pl
@@ -90,12 +90,11 @@ combined_yield([X|Rest], Result) :-
 	combined_yield(Rest, ResultRest),
 	append_with_space(ResultStart, ResultRest, Result).
 
-% We explicitly work with lists of char codes, even in swipl-v7 where double-quoted text is no longer interpreted this way
 append_with_space(X,[],X).
 append_with_space([],X,X).
 append_with_space(X,Y,Result) :-
-	char_code(' ', Space),
-	append(X, [Space|Y], Result).
+	append(X, " ", X1),
+	append(X1, Y, Result).
 
 end_tree(Stream) :-
 	format(Stream, "\\end{picture}~n", []),
